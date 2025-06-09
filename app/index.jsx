7ef1708b74de9caa3803afe1f3b97ffc4375649e
kylorc0624 <kylorc0624@vt.edu>
2025-06-08 23:03:39 -0400

    logo image test

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+
+import Logo from '../assets/img/logo.png'
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={Logo}/>
       <Text>Home</Text>
     </View>
   )
@@ -11,4 +14,10 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
