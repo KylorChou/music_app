6c790551bfa2100a0aa3384e63aca5213f3929db
kylorc0624 <kylorc0624@vt.edu>
2025-06-09 23:14:23 -0400

    updated index and added homepage

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,40 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 import Logo from '../assets/img/logo.png'
 
-const Home = () => {
+const {width, height } = Dimensions.get('window');
+
+const Welcome = () => {
+
   return (
     <View style={styles.container}>
-      <Image source={Logo}/>
-      <Text>Home</Text>
+      <Image source={Logo} style={styles.img} resizeMode="contain"/>
+
+      <Text style={styles.title}>Welcome!</Text>
+
+      <Link href="/home">Sign-in</Link>
     </View>
   )
 }
 
-export default Home
+export default Welcome
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
+  img: {
+    width: 0.3 * width,
+    height: 0.3 * height,
+    transform: [{translateY: -250}]
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    transform: [{translateY: -275}]
+  },
 })
